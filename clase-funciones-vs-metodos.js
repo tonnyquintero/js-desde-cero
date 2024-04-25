433cef0f9122ad142bdf20e40a60833e0d1a8b17
tonny187 <tonyoanthony@gmail.com>
2024-04-25 06:07:44 -0500

    adding two more files

---
+++ clase-funciones-vs-metodos.js
@@ -0,0 +1,45 @@
+// Capacidades que tienen las funciones al igual que otros objetos
+
+// 1. Pasar funciones como argumentos -> callback
+function a () {}
+function b (a) {}
+b(a)
+
+// 2. Retornar funciones
+
+function a () {
+function b () {}
+return b
+}
+
+// 3. Asignar funciones a variables -> Expresión de funcion
+
+const a = function() {}
+
+// 4. Tener propiedades y metodos
+
+function a () {}
+const obj = {}
+a.call(obj)
+
+// 5. Anidar funciones -> Nested functions
+
+function a () {
+    function b () {
+        function c () {}
+        c()
+    }
+    b()
+    }
+a()
+
+// 6. Es posible almacenar funciones en objetos
+
+const rocket = {
+    name: 'Falcon 9',
+    launchMessage: function launchMessage () {
+        console.log('positive');
+    }
+}
+
+rocket.launchMessage()
