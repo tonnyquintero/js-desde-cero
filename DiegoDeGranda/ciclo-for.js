14641fafd15162169327003980288b44c9d0a96e
tonny187 <tonyoanthony@gmail.com>
2024-04-27 12:14:50 -0500

    adding a new file

---
+++ DiegoDeGranda/ciclo-for.js
@@ -0,0 +1,12 @@
+
+
+for (let variable = 1; variable < 11; variable++) {
+    console.log(variable);;
+    
+}
+
+let list = ['eat', 'sleep', 'code', 'repeat']
+
+for (let lista = 0; lista < list.length ; lista++) {
+    console.log(list[lista]);
+}
