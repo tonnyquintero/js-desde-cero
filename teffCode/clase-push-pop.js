d46004a40ae79d2265b612d1524b67d5bd8470ce
tonny187 <tonyoanthony@gmail.com>
2024-04-30 09:23:15 -0500

    adding a new file

--- teffCode/clase-push-pop.js
+++ teffCode/clase-push-pop.js
@@ -5,12 +5,47 @@ const countries = ['USA', 'Colombia', 'Mexico', 'UK']
 
 const newCountries = countries.push('Canada', 'Australia')
 
-console.log(countries);
-console.log(newCountries);
 
 // pop()
 
 const removedCountry = countries.pop()
 
-console.log(countries);
-console.log(removedCountry);
+// Practic Excersice
+
+const bookCart = []
+
+const ADD_BOOK_ACTION = 'addBook'
+const DELETE_BOOK_ACTION = 'deleteBook'
+const SHOW_BOOKS_ACTION = 'showBook'
+
+function bookPerformance (action, bookName) {
+    switch (action) {
+        case ADD_BOOK_ACTION:
+            bookCart.push(bookName)
+            break;
+        case DELETE_BOOK_ACTION:
+             if (bookCart.length === 0) {
+                console.log('The cart is empty');
+             } else {
+                const deletedBook = bookCart.pop()
+                console.log(`Deleted book: ${deletedBook}`);
+             }
+            break;
+        case SHOW_BOOKS_ACTION:
+            console.log(bookCart);
+             break;
+        default:
+            console.log('invalid option please choose a right function');
+    }
+}
+
+bookPerformance(ADD_BOOK_ACTION, '100 años de soledad')
+bookPerformance(SHOW_BOOKS_ACTION)
+bookPerformance(ADD_BOOK_ACTION, 'Ensayo sobre la ceguera')
+bookPerformance(SHOW_BOOKS_ACTION)
+bookPerformance(DELETE_BOOK_ACTION)
+bookPerformance(SHOW_BOOKS_ACTION)
+bookPerformance(ADD_BOOK_ACTION, 'metamorfosis')
+bookPerformance(SHOW_BOOKS_ACTION)
+bookPerformance(DELETE_BOOK_ACTION)
+bookPerformance(SHOW_BOOKS_ACTION)
