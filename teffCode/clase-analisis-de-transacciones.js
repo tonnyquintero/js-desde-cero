c514874662e726ef0fd07da8f71bb794a9b7bf96
tonny187 <tonyoanthony@gmail.com>
2024-05-06 18:20:36 -0500

    adding a new file

--- teffCode/clase-analisis-de-transacciones.js
+++ teffCode/clase-analisis-de-transacciones.js
@@ -24,3 +24,47 @@ console.log('largest Transaction: ', largestTransaction);
 
 const purchaseTransactions = transactions.filter(transaction => transaction.amount < 0)
 console.log('purchaseTransactions: ', purchaseTransactions);
+
+// 4. Find a transaction by description 
+
+function specificTransaction (description) {
+    const result = transactions.find(trans => trans.description == description)
+   // return result ? console.log(result) : console.log('not found'); Esta linea igual sirve es con este condicional
+   if (result) {
+    console.log('Specific Transaction: ', result)
+   } else {
+    console.log('Transaction not found');
+   }
+}
+
+console.log(specificTransaction('Salary Deposit'));
+
+// 5. Find the index of a transaction by amount
+
+function indexAmountTransaction (monto) {
+    const resultado = transactions.findIndex(trans => trans.amount === monto)
+   // return resultado ? console.log('Transaction Amount: ', resultado) : console.log('Amount transaction not found');
+    if (resultado === -1) {
+        console.log('Amount transaction not found') 
+    } else {
+        console.log(`Index of Transaction Amount ${monto} is ${resultado}`);
+    }
+}
+
+console.log(indexAmountTransaction(-100));
+
+// 6. Update Transaction Description
+
+
+function updateTransactionDescriptionByType(transactionsArray) {
+    transactionsArray.forEach(transaction => {
+      if (transaction.amount < 0) {
+        transaction.description = `Expense: ${transaction.description}`
+      } else {
+        transaction.description = `Income: ${transaction.description}`
+      }
+    });
+    return transactionsArray;
+  }
+
+  console.log(updateTransactionDescriptionByType(transactions));
